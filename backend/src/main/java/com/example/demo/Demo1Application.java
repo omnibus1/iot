8f7b86aaa80b00c9cb3ab2dfbf8c1637ac9c7e30
Bodzio-2 <dungeonraiderpl@gmail.com>
2024-01-26 01:11:31 +0100

    i dunno

--- backend/src/main/java/com/example/demo/Demo1Application.java
+++ backend/src/main/java/com/example/demo/Demo1Application.java
@@ -1,22 +1,40 @@
 package com.example.demo;
 
 import com.azure.messaging.eventgrid.EventGridEvent;
+import com.example.demo.Device.Device;
+import com.example.demo.Device.DeviceRepository;
 import com.example.demo.EventHandler.EventHandler;
+import com.example.demo.Reading.Reading;
+import com.example.demo.Reading.ReadingRepository;
+import com.nimbusds.jose.shaded.gson.Gson;
+import com.nimbusds.jose.shaded.gson.reflect.TypeToken;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.jms.annotation.EnableJms;
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.jms.core.JmsTemplate;
 import org.springframework.messaging.Message;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import java.time.LocalDateTime;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.function.Consumer;
 
 @SpringBootApplication
 public class Demo1Application {
+
+    @Autowired
+    DeviceRepository deviceRepository;
+
+    @Autowired
+    ReadingRepository readingRepository;
 
     @Autowired
     EventHandler eventHandler;
@@ -26,14 +44,52 @@ public class Demo1Application {
         SpringApplication.run(Demo1Application.class, args);
     }
 
-    @Bean
-    public Consumer<Message<String>> consume() {
-        return message -> {
-            List<EventGridEvent> eventData = EventGridEvent.fromString(message.getPayload());
-            eventData.forEach(event -> {
-                eventHandler.handleEvent(event);
-            });
-        };
+    @JmsListener(destination = "que2115", containerFactory = "jmsListenerContainerFactory")
+    public void receiveMessage(String message) {
+        try {
+            String[] values = message.split(",");
+
+            StringBuilder result = new StringBuilder();
+
+            for (String value : values) {
+                // Convert each value to integer
+                int intValue = Integer.parseInt(value.trim());
+
+                // Append the character corresponding to the ASCII code
+                result.append((char) intValue);
+            }
+
+            // Print or use the resulting string
+            System.out.println(result.toString());
+////        String str = new String(bytes, StandardCharsets.UTF_8);
+//         String[] w = message.split(",");
+//         List<Byte> b= new ArrayList<>();
+//         for(String s: w){
+//             b.add(Integer.parseInt(s));
+//         }
+            Map<String, Object> retMap = new Gson().fromJson(
+                    String.valueOf(result), new TypeToken<HashMap<String, Object>>() {}.getType()
+            );
+
+            Device device = deviceRepository.getDeviceBySn(retMap.get("sn").toString());
+
+
+            LocalDateTime currentDateTime = LocalDateTime.now();
+
+            //reading.setDateTime(currentDateTime);
+            //reading.setReading(retMap.get("pm25").toString());
+            //reading.setDevice(device);
+            //reading.setDevice(device);
+            Reading reading = new Reading(null, device, retMap.get("pm25").toString(), currentDateTime);
+
+
+            readingRepository.saveReading(reading);
+
+            LOGGER.info("Message received: {}", result.toString());
+        }
+        catch (Exception e){
+            LOGGER.info("ERROR");
+        }
     }
     @Bean
     public PasswordEncoder encoder(){
